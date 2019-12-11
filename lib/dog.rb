d5f8aae6ac3f4dff628ecd453f7e7f73474da3cf
kkirby16 <kkirby16@my.whitworth.edu>
2019-12-10 17:25:43 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog 
+
+    def bark 
+    puts "Woof!"
+    end 
+
+    def sit 
+    puts "The Dog is sitting"
+    end 
+end 
+
+#will need the .new when the class takes arguments into the class.
+
